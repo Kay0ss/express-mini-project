7e501e3919d19ca81771b5997e2c4f9d5c46c602
Kay Oss <mne36325i@gmail.com>
2021-10-20 10:37:06 -0500

    final commit

---
+++ routes/diagnostics.js
@@ -0,0 +1,26 @@
+const diagnostics = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const app = require('.');
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+ 
+
+// GET Route for retrieving diagnostic information
+
+diagnostics.get('/', (req, res) => {
+  // TODO: Logic for sending all the content of db/diagnostics.json
+  readFromFile('./db/diagnostics.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a error logging
+diagnostics.post('/', (req, res) => {
+  // TODO: Logic for appending data to the db/diagnostics.json file
+  if (req.body) {
+
+    readAndAppend(req.body, './db/diagnostics.json');
+    res.json(`Diagnostic added successfully 🚀`);
+  } else {
+    res.error('Error in adding diagnostic');
+  }
+});
+
+module.exports = diagnostics;
